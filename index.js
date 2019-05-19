1e682be777810491e2a2acdfcab1cc8e0705dd54
Jonathan Perret <j-github@jonathanperret.net>
2019-05-19 22:14:08 +0200

    Very basic client with download capability

---
+++ index.js
@@ -0,0 +1,82 @@
+var form = document.getElementById('basic-form');
+var dbx;
+
+function parseHash() {
+  var re = /(.*?)=([^\&]*)&?/gi;
+  var ret = {};
+  var match;
+  while (match = re.exec(document.location.hash.slice(1))) {
+    ret[match[1]] = decodeURIComponent(match[2]);
+  }
+  return ret;
+}
+
+var query = parseHash();
+
+var filesList = document.getElementById('files');
+
+dbx = new Dropbox.Dropbox({ accessToken: query.token, fetch: fetch });
+
+listFolder(query.path || '');
+
+window.addEventListener('popstate', function() {
+  query = parseHash();
+  listFolder(query.path || '');
+});
+
+function listFolder(path) {
+  dbx.filesListFolder({path: path})
+    .then(function(response) {
+      displayFiles(response.entries);
+    })
+    .catch(function(error) {
+      console.error(error);
+      alert(error.toString());
+    });
+}
+
+function displayFiles(files) {
+  filesList.innerHTML = '';
+  for (var i = 0; i < files.length; i++) {
+    var li = document.createElement('li');
+
+    var a = document.createElement('a');
+    var filetype = files[i]['.tag'];
+    var filepath = files[i].path_lower;
+    a.setAttribute('filepath', filepath);
+    a.setAttribute('filetype', filetype);
+
+    if (filetype === 'folder') {
+      a.setAttribute('href', '#token=' + query.token + '&path=' + encodeURIComponent(filepath));
+    } else {
+      a.setAttribute('href', '#');
+    }
+
+    a.appendChild(document.createTextNode(files[i].name + (filetype === 'folder' ? '/' : '')));
+
+    li.appendChild(a);
+
+    filesList.appendChild(li);
+  }
+}
+
+filesList.addEventListener('click', function(evt) {
+  var clicked = evt.srcElement;
+  if (clicked) {
+    var filepath = clicked.getAttribute('filepath');
+    if (filepath) {
+      var filetype = clicked.getAttribute('filetype');
+      if (filetype === 'folder') {
+        listFolder(filepath);
+      } else {
+        evt.preventDefault();
+        dbx.filesGetTemporaryLink({ path: filepath })
+        .then(function(result) {
+          clicked.style.color = 'green';
+          clicked.setAttribute('href', result.link);
+          clicked.removeAttribute('filepath');
+        });
+      }
+    }
+  }
+});
